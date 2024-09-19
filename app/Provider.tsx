ee77790da5ee7cb540c1c2af0674628833858019
Aditya Daksh <aditya2012058@akgec.ac.in>
2024-09-19 21:19:28 +0530

    Refactor Provider component to use Liveblocks for real-time collaboration

---
+++ app/Provider.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+import Loader from "@/components/Loader";
+import { getClerkUsers } from "@/lib/actions/user.actions";
+import {
+  LiveblocksProvider,
+  ClientSideSuspense,
+} from "@liveblocks/react/suspense";
+
+const Provider = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div>
+      <LiveblocksProvider
+        authEndpoint="/api/liveblocks-auth"
+        resolveUsers={async ({ userIds }) => {
+          const users = await getClerkUsers({ userIds });
+          return users;
+        }}
+      >
+        <ClientSideSuspense fallback={<Loader />}>
+          {children}
+        </ClientSideSuspense>
+      </LiveblocksProvider>
+    </div>
+  );
+};
+
+export default Provider;
