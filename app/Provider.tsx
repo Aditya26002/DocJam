3a8c40ac78c9627dcd2f82018777419a23eef918
Aditya Daksh <aditya2012058@akgec.ac.in>
2024-09-25 02:15:24 +0530

    Refactor Provider component to include Sentry instrumentation for capturing request errors

--- app/Provider.tsx
+++ app/Provider.tsx
@@ -1,13 +1,15 @@
 "use client";
 
 import Loader from "@/components/Loader";
-import { getClerkUsers } from "@/lib/actions/user.actions";
+import { getClerkUsers, getDocumentUsers } from "@/lib/actions/user.actions";
+import { useUser } from "@clerk/nextjs";
 import {
   LiveblocksProvider,
   ClientSideSuspense,
 } from "@liveblocks/react/suspense";
 
 const Provider = ({ children }: { children: React.ReactNode }) => {
+  const { user: clerkUser } = useUser();
   return (
     <div>
       <LiveblocksProvider
@@ -15,6 +17,14 @@ const Provider = ({ children }: { children: React.ReactNode }) => {
         resolveUsers={async ({ userIds }) => {
           const users = await getClerkUsers({ userIds });
           return users;
+        }}
+        resolveMentionSuggestions={async ({ text, roomId }) => {
+          const roomUsers = await getDocumentUsers({
+            roomId,
+            currentUser: clerkUser?.emailAddresses[0].emailAddress!,
+            text,
+          });
+          return roomUsers;
         }}
       >
         <ClientSideSuspense fallback={<Loader />}>
